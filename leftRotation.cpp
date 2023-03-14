d27d8e1dd6926acf3760ddcb4e4c50e3381a7f73
Nour Ghazy <nourdin.dna@gmail.com>
2023-03-14 17:39:54 +0200

    Update leftRotation.cpp

--- leftRotation.cpp
+++ leftRotation.cpp
@@ -3,12 +3,23 @@
 
 using namespace std;
 
+// init a function that rotates each element to the left
 vector<int> rotateLeft(int d, vector<int> arr) {
+
+    // Lopping as long as number of rotations left is more than 0
     while(d > 0){
+        // Init. a sort of a pointer
+        // Shifting the first element of the vector to the end 
         arr.push_back(arr.front());
+
+        // Removing the iterator 
         arr.erase(arr.begin());
+
+        // After the rotation ends we decrement the number of rotations left
         d--;
     }
+
+    // Looping through the vector to print out each element
     for (int i = 0; i < arr.size(); i++) {
         cout << arr[i] << "";
     }
