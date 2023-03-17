10713a7bb84e6e15f94e5acdd4a4ed8bccd7dc88
Noureldin Ghazy <nourdin.dna@gmail.com>
2023-03-17 21:35:32 +0200

    added comments

--- stack.cpp
+++ stack.cpp
@@ -2,30 +2,37 @@
 using namespace std;
 
 int main(){
-  int t; 
+  //  variable with amount of queries
+  int t;
   cin >> t;
   stack<int> stc;
   
   while(t > 0){
-      int x;
+    
+    int x;
+    cin >> x;
+
+    // If input is  1 then push the number to the stack
+    if(x == 1){
       cin >> x;
-      if(x == 1){
-        cin >> x;
-        stc.push(x);
-      }
-      else if(x == 2){
-        if(!stc.empty())
-        stc.pop();
-      }
-      else{
-          if(stc.empty()){ 
-          cout << "Empty" << endl;
-          }
-          else{
-            cout << stc.top();
-          }
-      }
-      t--;
-   }
+      stc.push(x);
+    }
+    // If input is 2 and the stack is not empty then remove the top element
+    else if(x == 2){
+      if(!stc.empty())
+      stc.pop();
+    }
+
+    // Else just print the top element
+    else{
+        if(stc.empty()){ 
+        cout << "Empty" << endl;
+        }
+        else{
+        cout << stc.top();
+        }
+    }
+    t--;
+ }
   return 0;
 }
